b519ddaf692802f054e0c664591372608448d108
Changbin Park <gh4ck3r@gmail.com>
2018-12-26 09:43:45 +0900

    Update todaysppc.user.js

--- todaysppc.user.js
+++ todaysppc.user.js
@@ -3,12 +3,12 @@
 // @namespace   http://www.github.com/gh4ck3r
 // @description Customize and remove Ad. from Todays PPC site
 // @include     https://www.todaysppc.com/*
-// @version     8
+// @version     9
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
 
-document.styleSheets[0].insertRule('body>table:first-of-type>tbody>tr:first-of-type, zeroboard, #right_ad {display: none;}');
+document.styleSheets[0].insertRule('body>table:first-of-type>tbody>tr:first-of-type, ins.adsbygoogle, zeroboard>zeroboard>p:first-of-type, #right_ad {display: none;}');
 
 //var adSelectors = [];
 //document.querySelectorAll(adSelectors.join()).forEach(elem => elem.style.display='none');
