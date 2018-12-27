9881e13a7bf2f1e46007ce338ecf727ae443646e
Changbin Park <gh4ck3r@gmail.com>
2018-12-27 14:13:14 +0900

    Move hidden layered menu to proper position

--- todaysppc.user.js
+++ todaysppc.user.js
@@ -3,16 +3,21 @@
 // @namespace   http://www.github.com/gh4ck3r
 // @description Customize and remove Ad. from Todays PPC site
 // @include     https://www.todaysppc.com/*
-// @version     9
+// @version     10
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
 
-document.styleSheets[0].insertRule('body>table:first-of-type>tbody>tr:first-of-type, ins.adsbygoogle, zeroboard>zeroboard>p:first-of-type, #right_ad {display: none;}');
-
-//var adSelectors = [];
-//document.querySelectorAll(adSelectors.join()).forEach(elem => elem.style.display='none');
+(function(){
+  const css = document.styleSheets[0];
+  if (!css) return;
+  
+  [
+    'body>table:first-of-type>tbody>tr:first-of-type, ins.adsbygoogle, zeroboard>zeroboard>p:first-of-type, #right_ad {display: none;}',
+    '#layer1, #layer1 ~ div { top: 40px !important;}',
+  ].forEach(css.insertRule.bind(css));
+})()
 
 const workDocument = document.querySelector('#work').contentDocument;
 workDocument.querySelectorAll('#layer50,body>font:last-of-type')
-	.forEach(elem => elem.style.display='none');
+  .forEach(elem => elem.style.display='none');
