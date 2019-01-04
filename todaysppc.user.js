32637b4367dbc165e7419b6cd5fae16e3098fcaf
Changbin Park <gh4ck3r@gmail.com>
2019-01-04 11:40:43 +0900

    Update todaysppc.user.js

--- todaysppc.user.js
+++ todaysppc.user.js
@@ -3,7 +3,7 @@
 // @namespace   http://www.github.com/gh4ck3r
 // @description Customize and remove Ad. from Todays PPC site
 // @include     https://www.todaysppc.com/*
-// @version     11
+// @version     12
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
@@ -21,13 +21,59 @@
       'nav',	// Left navigation pane and button on mobile
       'body>center ~ div[style="display:inline-block;padding:1px;width:100%;height:100px;position:-webkit-sticky;top:10px; border:1px;font-size:9pt;background:#eeeeee"]',	// "See Mobile version" at bottom on mobile
       '#ljUJfypZLugD', // right ad on mobile
+      'body > table > tbody > tr > td[cellspadding="0"]:last-of-type',	// Right side pane
+      'body > table > colgroup > col:last-of-type',											// Right side pane
     ].join(',') + '{display: none !important;}',
     '#layer1, #layer1 ~ div { top: 40px !important;}',
+    '.slideToggle {transition: height .3s; overflow-y:hidden; }',
+    '.slideToggleBtn {cursor: pointer;}',
+    '.toggleHidden {height: 0px !important;, padding-top: 0px !important; padding-bottom: 0px !important; margin-top: 0px !important; margin-bottom: 0px !important;}',
   ].forEach(css.insertRule.bind(css));
+})();
+
+(function() {
+  const commentsFrame = document.querySelector('#work');
+  if (commentsFrame instanceof HTMLIFrameElement) {
+    const {contentDocument} = commentsFrame;
+    if (contentDocument) {
+      contentDocument.querySelectorAll('#layer50,body>font:last-of-type')
+        .forEach(elem => elem.style.display='none');
+    }
+  }
+})();
+
+
+(function() {
+  const leftSideBar = document.querySelector('#left-side-bar');
+  if (!leftSideBar) return;
+
+  const appendIntoLeftSideBar = leftSideBar.appendChild.bind(leftSideBar);
+  document.querySelectorAll('.side-bar-category').forEach( category => {
+    const list = category.nextElementSibling;
+    if (!list.classList.contains('side-bar-list')) return;
+
+    if (!leftSideBar.contains(category)) {
+      [category, list].map(appendIntoLeftSideBar);
+    }
+
+    makeSlideToggle(category, list);
+    list.classList.toggle('toggleHidden');
+  });
+  
+  document.querySelectorAll('table[width="1330"]').forEach(t => t.width=1000);
 })()
 
-const workDocument = document.querySelector('#work').contentDocument;
-if (workDocument) {
-  workDocument.querySelectorAll('#layer50,body>font:last-of-type')
-    .forEach(elem => elem.style.display='none');
+function makeSlideToggle(toggler, content)
+{
+  if (![toggler, content].every(e => e instanceof HTMLElement)) return false;
+
+  const {style, offsetHeight, classList} = content;
+  classList.add('slideToggle');
+  style.height = offsetHeight;
+
+  toggler.classList.add('slideToggleBtn');
+  toggler.addEventListener('click', () => {
+    classList.toggle('toggleHidden');
+  });
+  return true;
 }
